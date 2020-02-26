abd2e8763bb2a1e5b1117f1c6008866cf16e168f
Tyler Campbell <tcampbell@hy-vee.com>
2020-02-26 14:16:42 -0600

    removed commentted out material

--- src/NavMenu.js
+++ src/NavMenu.js
@@ -1,10 +1,6 @@
 import React, {Component} from "react";
 import {Link, Route, Switch} from "react-router-dom";
 import './NavMenu.css';
-import Page1 from "./Page1";
-import Page2 from "./Page2";
-import Page3 from "./Page3";
-
 
 class NavMenu extends Component {
     render() {
